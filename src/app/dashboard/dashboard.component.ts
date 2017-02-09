187d4728140b01b015fbcecb01948c817c2258db
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-09 10:59:59 +0100

    Property und Event Bindings

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,15 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books: string[];
+  books: Book[];
 
   constructor() { }
 
   ngOnInit() {
     this.books = [
-      'AngularJS 1.x',
-      'Angular'
+      new Book('123456789', 'Angular', 'Zurück in die Zukunft', 5),
+      new Book('987654321', 'AngularJS 1.x', 'Oldie but goldie', 2)
     ];
+  }
+
+  reorderBooks(book: Book) {
+    console.log(book.title);
+    this.books.sort((a, b) => b.rating - a.rating);
   }
 
 }
